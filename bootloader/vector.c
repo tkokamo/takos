6ebdaa1701ce0ee4939d23a891628b17390bb292
Takuya Okamoto <characterokamo@gmail.com>
2014-06-18 17:38:59 +0900

    Day 7 bootloader part compilation succeeded

--- bootloader/vector.c
+++ bootloader/vector.c
@@ -1,6 +1,9 @@
 #include "defines.h"
 
 extern void start(void);	/*start up*/
+extern void intr_softerr(void); /*software error*/
+extern void intr_syscall(void); /*system call*/
+extern void intr_serintr(void); /*serial interrupt*/
 
 /*
  *	interrupt vecrot
@@ -8,11 +11,13 @@ extern void start(void);	/*start up*/
  */
 void (*vectors[])(void) = {
  start, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+ intr_syscall, intr_softerr, intr_softerr, intr_softerr,
  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
- NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
- NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+ intr_serintr, intr_serintr, intr_serintr, intr_serintr,
+ intr_serintr, intr_serintr, intr_serintr, intr_serintr, 
+ intr_serintr, intr_serintr, intr_serintr, intr_serintr,
 };
